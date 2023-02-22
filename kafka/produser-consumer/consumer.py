92d20d35dd72b7f1479a80f330e4f7fdb810c8b4
Artem <boss_aaa@ukr.net>
2023-02-22 16:03:46 +0200

    Update consumer.py

--- kafka/produser-consumer/consumer.py
+++ kafka/produser-consumer/consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(
     'test-topic',
-     bootstrap_servers=['192.168.1.5:9092'],
+     bootstrap_servers=['192.168.1.5:9093'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group-testtopic')
